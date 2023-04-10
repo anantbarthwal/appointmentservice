08aacca4106ae5590ab7df370b02dafa7078d1a5
Anant Barthwal <anant.barthwal@citiustech.com>
2023-04-10 21:33:15 +0530

    applicationservice: enabled eurekaclient

--- src/main/java/com/citihospital/appointmentservice/AppointmentserviceApplication.java
+++ src/main/java/com/citihospital/appointmentservice/AppointmentserviceApplication.java
@@ -2,9 +2,11 @@ package com.citihospital.appointmentservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableEurekaClient
 @ComponentScan(basePackages = "com.citihospital")
 public class AppointmentserviceApplication {
 
