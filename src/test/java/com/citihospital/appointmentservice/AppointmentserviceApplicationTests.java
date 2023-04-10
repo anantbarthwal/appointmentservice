1aae2786b62e46ab832867b459e824edb2fabc85
Anant Barthwal <anant.barthwal@citiustech.com>
2023-04-10 16:37:04 +0530

    initial commit

---
+++ src/test/java/com/citihospital/appointmentservice/AppointmentserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.citihospital.appointmentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppointmentserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
